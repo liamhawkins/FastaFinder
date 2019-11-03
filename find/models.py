fba6376aff74f99c2df937bb5fff03b336acf8ab
Liam Hawkins <liamhawkinscu@gmail.com>
2019-11-03 16:32:09 -0500

    Modify Fasta model so that is has Accession field and description field

--- find/models.py
+++ find/models.py
@@ -5,12 +5,13 @@ class Query(models.Model):
     first_time_queried = models.DateTimeField(auto_now_add=True)
     most_recent_time_queried = models.DateTimeField(auto_now=True)
     raw_query = models.CharField(max_length=50)
-    fasta = models.ForeignKey('Fasta', null=True, blank=True, on_delete=models.DO_NOTHING)
-    user = models.ForeignKey('User', related_name='user_queries', on_delete=models.DO_NOTHING)
+    fasta = models.ForeignKey("Fasta", null=True, blank=True, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey("User", related_name="user_queries", on_delete=models.DO_NOTHING)
 
 
 class Fasta(models.Model):
-    accession = models.CharField(max_length=255)
+    accession = models.CharField(max_length=15)
+    description = models.CharField(max_length=255)
     sequence = models.TextField()
     source = models.CharField(max_length=255)
 
@@ -19,4 +20,3 @@ class User(models.Model):
     first_time_queried = models.DateTimeField(auto_now_add=True)
     most_recent_time_queried = models.DateTimeField(auto_now=True)
     ip = models.GenericIPAddressField(blank=True, null=True)
-
