a64d735a288cb4723501e1111b04ab3224fbf059
Liam Hawkins <liamhawkinscu@gmail.com>
2019-11-03 16:34:10 -0500

    Implement basic logic for taking query and checking all sources

--- find/views.py
+++ find/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from ipware import get_client_ip
 
-from find.models import User, Query
+from find.models import User, Query, Fasta
+from find.sources import Uniprot
+
+sources = [Uniprot]
 
 
 def log_user(request):
@@ -12,16 +15,20 @@ def log_user(request):
     return user
 
 
-def get_fasta(accession):
+def get_fasta(query):
     # TODO: Implement
-    return None
+    for source in sources:
+        if source.is_valid(query):
+            fasta_tuple = source.get(query)
+            fasta, _ = Fasta.objects.get_or_create(**fasta_tuple._asdict())
+            return fasta
 
 
 def query(request, raw_query=None):
     user = log_user(request)
     if not raw_query:
-        return HttpResponse('Invalid Query')
+        return HttpResponse("Invalid Query")
     else:
         fasta = get_fasta(raw_query)
-        query, created = Query.objects.get_or_create(raw_query=raw_query, fasta=fasta, user=user)
-        return HttpResponse('{}'.format(raw_query))
+        query, _ = Query.objects.get_or_create(raw_query=raw_query, fasta=fasta, user=user)
+        return HttpResponse("{}\n{}".format(query.fasta.description, query.fasta.sequence))
