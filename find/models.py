202196c06612453f66ecbeae04f6951e869b409f
Liam <liamhawkinscu@gmail.com>
2019-11-06 15:49:06 -0500

    Add num_queries count to user and queries models

--- find/models.py
+++ find/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Query(models.Model):
     first_time_queried = models.DateTimeField(auto_now_add=True)
     most_recent_time_queried = models.DateTimeField(auto_now=True)
+    num_queries = models.PositiveIntegerField(default=1)
     raw_query = models.CharField(max_length=50)
     fasta = models.ForeignKey("Fasta", null=True, blank=True, on_delete=models.DO_NOTHING)
     user = models.ForeignKey("User", related_name="user_queries", on_delete=models.DO_NOTHING)
@@ -18,6 +19,7 @@ class Fasta(models.Model):
 class User(models.Model):
     first_time_queried = models.DateTimeField(auto_now_add=True)
     most_recent_time_queried = models.DateTimeField(auto_now=True)
+    num_queries = models.PositiveIntegerField(default=1)
     ip = models.GenericIPAddressField(blank=True, null=True)
 
 
