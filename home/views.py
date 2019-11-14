f42d305480b55d75b64e0d2e7f29eecd3f1193e3
Liam <liamhawkinscu@gmail.com>
2019-11-14 13:29:46 -0500

    Add home page input form

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def main(request):
-    return HttpResponse('Usage: 127.0.0.1:8000/&ltINSERT QUERY HERE&gt')
+    return render(request, 'home/home.html')
