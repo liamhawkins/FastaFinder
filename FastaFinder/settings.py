0f4df792d4e422850e837e712762860b47312ccb
Liam Hawkins <liamhawkinscu@gmail.com>
2020-07-05 15:38:30 -0400

    Move settings

--- FastaFinder/settings.py
+++ FastaFinder/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -81,7 +82,7 @@ WSGI_APPLICATION = 'FastaFinder.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../FastaFinder/db.sqlite3'),
     }
 }
 
